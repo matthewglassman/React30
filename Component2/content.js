8e2e921557eb311aaa8278b7ee2455ffc385affb
matthewglassman <matthewglassman78@gmail.com>
2017-05-17 16:09:30 -0400

    Creating a child Content Component

---
+++ Component2/content.js
@@ -0,0 +1,29 @@
+class Content extends React.Component{
+	render(){
+		return(
+			<div className="content">
+				<div className="line"></div>
+
+			{/* Timeline item */}
+
+				<div className="item">
+					<div className="avatar">
+						<img src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+						doug
+					</div>
+
+					<span className="time">
+						An hour ago
+					</span>
+					<p>Ate lunch</p>
+					<div className="commentCount">
+						2
+					</div>
+				</div>
+
+			{/* ... */}
+
+			</div>
+		)
+	}
+}

37f4922bb6259de86db14f4365efda7846c103b3
matthewglassman <matthewglassman78@gmail.com>
2017-05-17 15:56:13 -0400

    Creating Components with parent/children relationships.  App.js Wrapper component first

---
+++ Component2/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component{
+	render(){
+		return(
+			<div className="notificationsFrame">
+				<div className="panel">
+					{/* content will go here when rendered */}
+				</div>
+			</div>		
+		)
+	}
+}
